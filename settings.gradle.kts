d46816f8ed580411c688415d001cce6dbbeb05db
drahovac <47576387+drahovac@users.noreply.github.com>
2022-07-24 12:27:04 +0200

    Feature/init multiplatform (#1)
    * Ignore build folders
    
    * Initialize multiplatform project

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KtMultiNostr"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/bobmitchigan/com/Platform.kt
@@ -0,0 +1,5 @@
+package com.bobmitchigan.com
+
+expect class Platform() {
+    val platform: String
+}
