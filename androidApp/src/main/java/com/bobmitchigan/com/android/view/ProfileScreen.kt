2ba623b20735bca05994f4c8859131b76a230f79
Vaclav Drahorad <drahorad.vac@gmail.com>
2022-09-18 21:30:05 +0200

    Show profile image

--- androidApp/src/main/java/com/bobmitchigan/com/android/view/ProfileScreen.kt
+++ androidApp/src/main/java/com/bobmitchigan/com/android/view/ProfileScreen.kt
@@ -1,7 +1,9 @@
 package com.bobmitchigan.com.android.view
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
@@ -9,10 +11,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import co.touchlab.kermit.Logger
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.bobmitchigan.com.android.R
 import com.bobmitchigan.com.android.ui.components.LabelValue
 import com.bobmitchigan.com.android.ui.components.TextHeadline3
@@ -47,9 +53,25 @@ private fun ProfileContent(profiles: List<Profile>) {
 @Composable
 private fun ProfileCell(profile: Profile) {
     Card(Modifier.padding(16.dp)) {
-        Column(Modifier.padding(16.dp)) {
-            LabelValue(label = stringResource(id = R.string.author), value = profile.name)
-            LabelValue(label = stringResource(id = R.string.about), value = profile.about.orEmpty())
+        Row {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(profile.picture)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = stringResource(id = R.string.profile_avatar),
+                contentScale = ContentScale.Inside,
+                modifier = Modifier
+                    .padding(start = 16.dp, top = 16.dp)
+                    .size(32.dp)
+            )
+            Column(Modifier.padding(16.dp)) {
+                LabelValue(label = stringResource(id = R.string.author), value = profile.name)
+                LabelValue(
+                    label = stringResource(id = R.string.about),
+                    value = profile.about.orEmpty()
+                )
+            }
         }
     }
 }
