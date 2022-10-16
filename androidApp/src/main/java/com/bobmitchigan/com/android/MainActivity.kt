f1b5dcee8f8a4b073aa2d922f56fe8af8996e55d
Vaclav Drahorad <drahorad.vac@gmail.com>
2022-10-16 20:03:55 +0200

    Commit all

--- androidApp/src/main/java/com/bobmitchigan/com/android/MainActivity.kt
+++ androidApp/src/main/java/com/bobmitchigan/com/android/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -24,6 +25,7 @@ class MainActivity : ComponentActivity(), KoinComponent {
     private val detailViewModel: MainViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
 
         detailViewModel.init()
