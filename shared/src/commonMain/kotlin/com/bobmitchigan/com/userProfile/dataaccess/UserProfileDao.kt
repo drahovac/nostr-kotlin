e75bf953e9418a237070f7287827d72d20bb5958
Vaclav Drahorad <drahorad.vac@gmail.com>
2022-11-21 22:02:15 +0100

    Create profile

--- shared/src/commonMain/kotlin/com/bobmitchigan/com/userProfile/dataaccess/UserProfileDao.kt
+++ shared/src/commonMain/kotlin/com/bobmitchigan/com/userProfile/dataaccess/UserProfileDao.kt
@@ -1,6 +1,7 @@
 package com.bobmitchigan.com.userProfile.dataaccess
 
 import com.bobmitchigan.EventDatabase
+import com.bobmitchigan.UserProfileEntity
 import com.squareup.sqldelight.runtime.coroutines.asFlow
 import com.squareup.sqldelight.runtime.coroutines.mapToList
 
@@ -9,4 +10,6 @@ class UserProfileDao(database: EventDatabase) {
     private val db = database.userProfileQueries
 
     fun selectAll() = db.selectAll().asFlow().mapToList()
+
+    fun createUserProfile(entity: UserProfileEntity) = db.updateOrInsert(entity)
 }
--- shared/src/commonMain/kotlin/com/bobmitchigan/com/userProfile/dataaccess/UserProfileRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/com/bobmitchigan/com/userProfile/dataaccess/UserProfileRepositoryImpl.kt
@@ -3,6 +3,9 @@ package com.bobmitchigan.com.userProfile.dataaccess
 import com.bobmitchigan.UserProfileEntity
 import com.bobmitchigan.com.userProfile.domain.UserProfile
 import com.bobmitchigan.com.userProfile.domain.UserProfileRepository
+import com.soywiz.krypto.SecureRandom
+import com.soywiz.krypto.encoding.Hex
+import fr.acinq.secp256k1.Secp256k1
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
@@ -14,13 +17,43 @@ class UserProfileRepositoryImpl(
         about: String?,
         picture: String?
     ): Result<UserProfile> {
-        TODO("Not yet implemented")
+        return runCatching {
+            val private = createPrivateKey()
+            val public = createPublicKey(private)
+            val entity = UserProfileEntity(
+                pubkey = Hex.encode(public),
+                privkey = Hex.encode(private),
+                name = name,
+                about = about,
+                image = picture
+            )
+
+            dao.createUserProfile(entity)
+            entity.toDomain()
+        }
     }
+
+   private fun createPrivateKey(): ByteArray {
+        val bytes = ByteArray(32)
+        SecureRandom.nextBytes(bytes)
+        return bytes
+    }
+
+   private fun createPublicKey(privateKey: ByteArray) =
+        Secp256k1.pubKeyCompress(Secp256k1.pubkeyCreate(privateKey)).copyOfRange(1, 33)
 
     override fun getUserProfiles(): Flow<List<UserProfile>> {
         return dao.selectAll().map { it.toDomain() }
     }
 }
+
+private fun UserProfileEntity.toDomain()  = UserProfile(
+        pubKey = pubkey,
+        privateKey = privkey,
+        name = name,
+        about = about,
+        image = image
+    )
 
 private fun List<UserProfileEntity>.toDomain(): List<UserProfile> {
     return map {
